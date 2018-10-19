dcdd9e0dde5355c16bafc5e935e3f227cf7096e5
Akash Hardia <aakashhardiya@gmail.com>
2018-10-19 23:30:12 +0530

    change: indentation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-let restaurantCache = 'cache1';
+let restaurantCache = 'mycache';
 
-let filesToCache = [
+let cacheFiles = [
     './index.html',
     './restaurant.html',
     './css/styles.css',
@@ -22,11 +22,11 @@ let filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-    console.log("[ServiceWorker] Installed")
+    console.log("ServiceWorker is Installed")
     e.waitUntil( //hold the service worker until following tasks are completed
         caches.open(restaurantCache).then(function(cache) {
-            console.log("[ServiceWorker] Caching cacheFiles");
-            return cache.addAll(filesToCache);
+            console.log("Caching cacheFiles");
+            return cache.addAll(cacheFiles);        
         })
         .catch(function(err) {
             console.log('[ServiceWorker] falied open the Cach ', err);
@@ -35,10 +35,10 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) { 
-    console.log("[ServiceWorker] Activated")
+    console.log("new ServiceWorker has been Activated");
     e.waitUntil( // 
-        caches.keys().then(function(cacheNames){ //.keys() gives all cache names with a promise
-            return Promise.all(cacheNames.filter(function(cacheName) { // wrap all promises in Promis.all() and wait its completion
+        caches.keys().then(function(cacheNames){    //.keys() will give all cache names
+            return Promise.all(cacheNames.filter(function(cacheName) { // wraps all promises in Promis.all() and wait for its completion
                 return cacheName.startsWith('restaurant-') && cacheName != restaurantCache;}) //  update the database
                 .map(function(cacheName){ //map and
                     return caches.delete(cacheName); // delete outdated caches
@@ -48,22 +48,20 @@ self.addEventListener('activate', function(e) {
     );
 });
 
-self.addEventListener('fetch', function (event) { // normal browser fetch(results come from cache)
-    console.log('[ServiceWorker] Fetch', event.request.url);
-    event.respondWith( // search for match in caches for this request
-      caches.match(event.request)// match request with what is in cache
-      .then(function (resp) { 
-        return resp || fetch(event.request) 
-      .then(function (response) {
-        return caches.open('cache1').then(function (cache) { //opens cache object 'cache1'
-          cache.put(event.request, response.clone()); // event.request and response are put in the opened cache
-          console.log('[ServiceWorker] New Data Cached', event.request.url);
-          return response;
+self.addEventListener('fetch', function (event) { 
+    console.log('Fetch by ServiceWorker', event.request.url);
+    event.respondWith( // searching for match in caches for request
+      caches.open('mycache').then(function (cache) {
+        return caches.match(event.request).then(function (res) { 
+            return res || fetch(event.request).then(function (response) {
+                  cache.put(event.request, response.clone()); 
+                  console.log('New Data Cached', event.request.url);
+                  return response;
           });
         });
       })
       .catch(function(err) {
-        console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+        console.log('Error in Fetching & Caching New Data', err);
         })
     );
 });
