bd35bbe7412f6eb047917f7acf7930b32d28a0df
Akash Hardia <aakashhardiya@gmail.com>
2018-10-20 10:33:58 +0530

    changes according to feedback

--- js/main.js
+++ js/main.js
@@ -220,7 +220,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = DBHelper.altForRestaurant(restaurant);          // alt for image
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
